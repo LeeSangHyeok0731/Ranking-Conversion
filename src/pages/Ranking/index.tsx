7f854b8982e8a3f0e22ee2b008af67d21e8f7d16
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-24 22:24:03 +0900

    배경색 설정

--- src/pages/Ranking/index.tsx
+++ src/pages/Ranking/index.tsx
@@ -1,3 +1,9 @@
+import styled from "styled-components";
+
+const Body = styled.div`
+  background-color: #fef9ef;
+`;
+
 export default function Ranking() {
-  return <></>;
+  return <Body></Body>;
 }
