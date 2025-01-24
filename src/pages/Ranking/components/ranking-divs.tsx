b3eca3d7281b4ee39997480639882366239932a7
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-24 22:51:02 +0900

    랭크와 이름 표시

--- src/pages/Ranking/components/ranking-divs.tsx
+++ src/pages/Ranking/components/ranking-divs.tsx
@@ -6,6 +6,8 @@ const Body = styled.div`
   height: 102px;
   background-color: #f3f3f3;
   border-radius: 16px;
+  display: flex;
+  align-items: center;
 `;
 
 const RankingColor = styled.div`
@@ -13,15 +15,19 @@ const RankingColor = styled.div`
   height: 102px;
   background-color: #f9d85e;
   clip-path: polygon(0 0, 100% 0, 70% 100%, 0% 100%);
+  border-radius: 16px 0 0 16px;
+  position: absolute;
 `;
 
 export default function RankingDiv() {
   const [name, setName] = useState<string>("이세민");
+  const [rank, setRank] = useState<number>(0);
 
   return (
     <Body>
       <RankingColor />
-      <h1>{name}</h1>
+      <h1 style={{ zIndex: 0 }}>{`#${rank}`}</h1>
+      <h1 style={{ zIndex: 0 }}>{name}</h1>
     </Body>
   );
 }
