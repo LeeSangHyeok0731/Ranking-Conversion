9d120fbd420c26e3ea27b51a0937a4f86913e1e9
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-25 23:54:18 +0900

    스타일 수정

--- src/pages/Ranking/index.tsx
+++ src/pages/Ranking/index.tsx
@@ -12,7 +12,8 @@ const Body = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 export default function Ranking() {
