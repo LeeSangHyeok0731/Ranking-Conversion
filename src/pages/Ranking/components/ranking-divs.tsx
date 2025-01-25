0d43b98786aa2554c9d5e06a0fad9f1ef0d74680
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-25 23:43:17 +0900

    비정상적인 ui css 적용 해결

--- src/pages/Ranking/components/ranking-divs.tsx
+++ src/pages/Ranking/components/ranking-divs.tsx
@@ -8,6 +8,7 @@ const Body = styled.div`
   border-radius: 16px;
   display: flex;
   align-items: center;
+  justify-content: center;
 `;
 
 const RankingColor = styled.div`
@@ -16,18 +17,43 @@ const RankingColor = styled.div`
   background-color: #f9d85e;
   clip-path: polygon(0 0, 100% 0, 70% 100%, 0% 100%);
   border-radius: 16px 0 0 16px;
-  position: absolute;
+`;
+
+const InfoBox = styled.div`
+  width: 1460px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+const FirstInfo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const SecondInfo = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 export default function RankingDiv() {
   const [name, setName] = useState<string>("이세민");
   const [rank, setRank] = useState<number>(0);
 
+  const [click, setClick] = useState<number>(0);
+
   return (
     <Body>
       <RankingColor />
-      <h1 style={{ zIndex: 0 }}>{`#${rank}`}</h1>
-      <h1 style={{ zIndex: 0 }}>{name}</h1>
+      <InfoBox>
+        <FirstInfo>
+          <h1 style={{ zIndex: 0 }}>{`#${rank}  `}</h1>
+          <h1 style={{ zIndex: 0 }}>{name}</h1>
+        </FirstInfo>
+        <SecondInfo>
+          <h1 style={{ zIndex: 0 }}>{click}</h1>
+          <h1 style={{ zIndex: 0 }}>클릭</h1>
+        </SecondInfo>
+      </InfoBox>
     </Body>
   );
 }
