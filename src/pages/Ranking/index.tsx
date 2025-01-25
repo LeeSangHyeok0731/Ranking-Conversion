987467ac9a4da58d16076d259300a12756b0cd64
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-25 23:52:55 +0900

    메타 데이터 설정 & 넘겨주기 설정

--- src/pages/Ranking/index.tsx
+++ src/pages/Ranking/index.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 import RankingDiv from "./components/ranking-divs";
 
+export type User = {
+  name: string;
+  rank: number;
+  click: number;
+};
+
 const Body = styled.div`
   background-color: #fef9ef;
   width: 100vw;
@@ -10,9 +16,39 @@ const Body = styled.div`
 `;
 
 export default function Ranking() {
+  const UserInfo: User[] = [
+    {
+      name: "김민수",
+      rank: 1,
+      click: 10000,
+    },
+    {
+      name: "이서현",
+      rank: 2,
+      click: 9999,
+    },
+    {
+      name: "박지훈",
+      rank: 3,
+      click: 9998,
+    },
+    {
+      name: "정유진",
+      rank: 4,
+      click: 9997,
+    },
+    {
+      name: "최영수",
+      rank: 5,
+      click: 9996,
+    },
+  ];
+
   return (
     <Body>
-      <RankingDiv />
+      {UserInfo.map((x) => {
+        return <RankingDiv name={x.name} rank={x.rank} click={x.click} />;
+      })}
     </Body>
   );
 }
